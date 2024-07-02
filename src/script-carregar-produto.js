13b646a776d9da5c3fc51eb1c21c7c59fa710bb4
rkzin <rkzinnine@gmail.com>
2024-07-01 22:11:29 -0300

    .

---
+++ src/script-carregar-produto.js
@@ -0,0 +1,72 @@
+// Importando o mysql2
+var mysql = require("mysql2");
+
+// Configuração para acessar o banco de dados
+var conexao = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "produtos",
+});
+
+// Conectando ao MySQL
+conexao.connect(function (error) {
+  if (error) {
+    console.log(`Ocorreu um erro ao conectar no banco de dados: ${error.code}`);
+    console.log(`Ocorreu um erro ao conectar no banco de dados: ${error.fatal}`);
+  } else {
+    console.log("Conectado ao banco de dados com sucesso!");
+  }
+});
+
+// Buscando os dados no MySQL
+var queryBuscar = "SELECT * FROM produto";
+conexao.query(queryBuscar, function (error, produtos) {
+  if (error) {
+    console.log(`Ocorreu um erro ao buscar os dados: ${error.code}`);
+    console.log(`Ocorreu um erro ao buscar os dados: ${error.fatal}`);
+  } else {
+    console.log("Os produtos encontrados foram:", produtos);
+    var listagemProdutos = document.getElementById("listagem-produtos");
+
+    // Criando os itens
+    produtos.forEach(produto => {
+      
+      // Criando as li 
+      var criarItem = document.createElement("li");
+      criarItem.setAttribute("id", "item");
+
+      // Criando a div
+      var criarIcones = document.createElement("div");
+      criarIcones.setAttribute("id", "icons");
+
+      // Criando os icone de renomear
+      var iconRename = document.createElement("img");
+      iconRename.setAttribute("id", "icon-rename");
+      iconRename.src = "../assets/icons/renomear.png";
+
+      // Criando o icone de deletar
+      var iconDelete = document.createElement("img");
+      iconDelete.setAttribute("id", "icon-delete");
+      iconDelete.src = "../assets/icons/deletar.png";
+
+      // Criando o conteúdo
+      criarItem.innerText = `${produto.nome} - ${produto.quantidade} - ${produto.codigo_produto} - ${produto.marca}`
+
+
+      // Adicionando os icones
+      criarIcones.appendChild(iconRename);
+      criarIcones.appendChild(iconDelete);
+
+      // Adicionando os icones ao item
+      criarItem.appendChild(criarIcones);
+      
+      // Adicionando os itens
+      listagemProdutos.appendChild(criarItem);  
+    })
+  }
+})
+
+
+var form = document.getElementById("form");
+form.addEventListener("submit", "keyup", adicionarProdutos);
